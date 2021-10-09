145ed1abd0fb932842e79507b67a65f8611c1fa2
Jake Lee <jake.lee@fdmgroup.com>
2021-10-09 10:52:31 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    while True:
+        user_string = input()
+        if (user_string != ""):
+            split_string = user_string.split(" ")
+            for word in split_string:
+                print(word)
+        else:
+            break
+
 
 if __name__ == '__main__':
     main()
